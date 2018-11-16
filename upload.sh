8153b6519124b140c4ae1d9105a7971dfb7ee14c
Matthew Lee <mattxlee@gmail.com>
2018-11-16 11:34:01 +0800

    Initial commit with all download files.

---
+++ upload.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+scp HowToMine-cn.pdf root@balancecoin.io:/var/www/balancecoin.io/download/HowToMine-cn.pdf
+scp ebtc_blockchain.torrent root@balancecoin.io:/var/www/balancecoin.io/download/ebtc_blockchain.torrent
+scp mac-blc-mining-tools.zip root@balancecoin.io:/var/www/balancecoin.io/download/mac-blc-mining-tools.zip
+scp mac-blc-wallet.dmg root@balancecoin.io:/var/www/balancecoin.io/download/mac-blc-wallet.dmg
+scp vc_redist.x64.exe root@balancecoin.io:/var/www/balancecoin.io/download/vc_redist.x64.exe
+scp win-blc-mining-tools.zip root@balancecoin.io:/var/www/balancecoin.io/download/win-blc-mining-tools.zip
+scp win-blc-wallet.zip root@balancecoin.io:/var/www/balancecoin.io/download/win-blc-wallet.zip
